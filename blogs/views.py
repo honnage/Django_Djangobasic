b08d51ccc69003769770c218374c88f953c9c4bb
honnage <hon_da-1998@hotmail.com>
2021-01-22 04:26:57 +0700

    Data to Template

--- blogs/views.py
+++ blogs/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html',
+        {
+            'name':'บทความท่องเที่ยวภาคเหนือ',
+            'author':'ภาสกร'
+        }
+    )
