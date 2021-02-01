b6d35e8d3f3d4b898e0dd57c70fd5017b538fcf6
honnage <hon_da-1998@hotmail.com>
2021-02-01 17:46:20 +0700

    If Statement

--- blogs/views.py
+++ blogs/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def hello(request):
     tags=['น้ำตก','ธรรมชาติ','หน้าฝน','ตากหมอก']
+    rating = 4
     return render(request, 'index.html',
         {
             'name':'บทความท่องเที่ยวภาคเหนือ',
             'author':'ภาสกร',
-            'tags':tags
+            'tags':tags,
+            'rating':rating
         }
     )
