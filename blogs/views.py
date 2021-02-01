3d2ac7ae85c9599fb8e4c701008f6d8c6e10b1ef
honnage <hon_da-1998@hotmail.com>
2021-02-01 17:35:46 +0700

    For loop Statement

--- blogs/views.py
+++ blogs/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello(request):
+    tags=['น้ำตก','ธรรมชาติ','หน้าฝน','ตากหมอก']
     return render(request, 'index.html',
         {
             'name':'บทความท่องเที่ยวภาคเหนือ',
-            'author':'ภาสกร'
+            'author':'ภาสกร',
+            'tags':tags
         }
     )
